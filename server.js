27a6be095996a159bbfdfd4c8f7829f392ed327c
Afnan <afnanzia97@yahoo.com>
2025-11-23 17:09:41 +0000

    Connected to MongoDB, made a middleware, then routed and finally started the server

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
-const express = require('express'); // importing express
-const mongoose = require('mongoose'); //importing mongoose
+const express = require('express'); 
+const mongoose = require('mongoose'); 
 const app = express();
-const routes = require('./routes'); // './' path to a file 
+const routes = require('./routes');  
 
-mongoose.connect('')
+// connecting to MongoDB
+
+mongoose.connect('mongodb://localhost27017/Restaurant').then(() => {
+    console.log('connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+// Middleware
+
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true})); 
+
+// Routes
+
+app.use('/api', routes); 
+
+ 
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+}); 
+
+
+
+ 
